f502a983cb74d18a6acf41b2148f081e7e453a19
Paul <paulhuwiler.info@gmail.com>
2025-03-27 08:45:12 +0100

    src

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var (
+	counter int
+	mu      sync.Mutex
+)
+
+func main() {
+	http.HandleFunc("/", servePage)
+	http.HandleFunc("/increment", incrementCounter)
+
+	fmt.Println("Serveur démarré sur http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func servePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	counter++
+	if counter == 12 {
+		counter = 0
+	}
+
+	mu.Unlock()
+	w.Write([]byte(strconv.Itoa(counter)))
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+### Ma première app WEB
+juste une série de petit test pour apprendre a maitriser Golang et le web.
