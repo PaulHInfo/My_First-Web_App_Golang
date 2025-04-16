b9b66cc6e29a44ac2505393b532f255243831a4c
Paul <paulhuwiler.info@gmail.com>
2025-04-16 10:39:04 +0200

    new code

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/test", test)
+	http.HandleFunc("/search", search)
+
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal(err)
@@ -16,5 +19,18 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello World")
 	fmt.Fprint(w, "Adieu") // affiche sur la page
+}
+func test(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello World")
+	fmt.Fprint(w, "test") // affiche sur la page
+}
 
+//requête http
+/*
+http://localhost:8000/search?t=go&p=1
+t & p sont des paramètre
+*/
+func search(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("salut")
+	fmt.Fprint(w, "salutttt")
 }
